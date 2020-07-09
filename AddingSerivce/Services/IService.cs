b465d0508fd4ac910a7a669c865d1aeed1a43bbe
Magnus <Renen@DESKTOP-88ILJBA>
2020-07-09 15:01:03 +0200

    Add project files.

---
+++ AddingSerivce/Services/IService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddingSerivce.Services
+{
+    public interface IService
+    {
+        string GetSomeResultBack();
+        Task<string> GetSomeResultBackAsync();
+
+        void CreateSomething();
+        Task CreateSomethingAsync();
+
+
+
+    }
+}
---
+++ AddingSerivce/Services/Service.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddingSerivce.Services
+{
+    public class Service : IService
+    {
+        public void CreateSomething()
+        {
+            Console.WriteLine("Created something");
+        }
+
+        public async Task CreateSomethingAsync()
+        {
+            var task = Task.Run(() => Console.WriteLine("Created something async"));
+            await task;
+        }
+
+        public string GetSomeResultBack()
+        {
+            return "This is the return value";
+        }
+
+        public async Task<string> GetSomeResultBackAsync()
+        {
+            return await SomeMethodThatIsAsync();
+        }
+
+        private async Task<string> SomeMethodThatIsAsync()
+        {
+            return "f";
+        }
+    }
+}
